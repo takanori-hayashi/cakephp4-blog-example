ede723dc59f039e6ad802c4cbbc34e90fe86ac0b
takanori-hayashi <mail@m.tknrh.me>
2020-08-26 15:31:54 +0900

    user機能追加

---
+++ config/Seeds/UsersSeed.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractSeed;
+use Authentication\PasswordHasher\DefaultPasswordHasher;
+
+/**
+ * Users seed.
+ */
+class UsersSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'username' => 'admin',
+                'password' => $this->_setPassword('admin'),
+                'created' => '2020-08-20 12:00:00',
+                'modified' => '2020-08-20 12:00:00',
+            ],
+            [
+                'username' => 'editor',
+                'password' => $this->_setPassword('editor'),
+                'created' => '2020-08-21 12:00:00',
+                'modified' => '2020-08-21 12:00:00',
+            ],
+        ];
+
+        $table = $this->table('users');
+        $table->insert($data)->save();
+    }
+
+    protected function _setPassword(string $password) : ?string
+    {
+        if (strlen($password) > 0) {
+            return (new DefaultPasswordHasher())->hash($password);
+        }
+    }
+}
